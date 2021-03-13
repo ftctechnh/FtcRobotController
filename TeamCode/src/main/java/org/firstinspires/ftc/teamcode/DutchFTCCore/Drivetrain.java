095884f60a472708b17567d528f6bfba5c0e7325
Ekiem13 <45897842+Ekiem13@users.noreply.github.com>
2021-03-13 15:21:33 +0100

    new code
    nieuwe code helemaal bijgewerkt vor 2021
    
    Co-Authored-By: Morgan720 <79752342+Morgan720@users.noreply.github.com>

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DutchFTCCore/Drivetrain.java
@@ -0,0 +1,43 @@
+package org.firstinspires.ftc.teamcode.Subsystems;
+
+import com.qualcomm.hardware.motors.RevRoboticsCoreHexMotor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Drivetrain {
+
+    public DcMotor left_front = null;
+    public DcMotor left_back = null;
+    public DcMotor right_front = null;
+    public DcMotor right_back = null;
+
+    public DcMotor intake_motor = null;
+    public DcMotor intake2_motor = null;
+    public DcMotor shooting_motor = null;
+
+    public void mechanum_hardware (HardwareMap m_hardwaremap) { //init
+
+        left_front = m_hardwaremap.get(DcMotor.class,"left_front");
+        left_back = m_hardwaremap.get(DcMotor.class,"left_back");
+        right_front = m_hardwaremap.get(DcMotor.class,"right_front");
+        right_back = m_hardwaremap.get(DcMotor.class,"right_back");
+
+        intake_motor = m_hardwaremap.get(DcMotor.class, "intake_motor");
+        intake2_motor = m_hardwaremap.get(DcMotor.class, "intake2_motor");
+        shooting_motor = m_hardwaremap.get(DcMotor.class, "shooting_motor");
+
+
+
+        left_front.setDirection(DcMotor.Direction.REVERSE);
+        left_back.setDirection(DcMotor.Direction.REVERSE);
+        right_front.setDirection(DcMotor.Direction.FORWARD);
+        right_back.setDirection(DcMotor.Direction.FORWARD);
+
+        intake_motor.setDirection(DcMotorSimple.Direction.FORWARD);
+        intake2_motor.setDirection(DcMotorSimple.Direction.FORWARD);
+        shooting_motor.setDirection(DcMotorSimple.Direction.FORWARD);
+    }
+
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DutchFTCCore/Robotcontrol.java
@@ -0,0 +1,45 @@
+package org.firstinspires.ftc.teamcode.Commands;
+
+import org.firstinspires.ftc.teamcode.Subsystems.Drivetrain;
+
+public class Robotcontrol {
+
+    public void mechanumdrive(Drivetrain m_drivetrain, double xDirection, double yDirection) { //Periodic
+
+        double r = Math.hypot(xDirection,yDirection);
+        double robotAngle = Math.atan2(yDirection,xDirection) - Math.PI/4;
+        double rightX = xDirection;
+        final double LeftFront = r * Math.cos(robotAngle)-rightX;
+        final double RightFront = r * Math.sin(robotAngle)+rightX;
+        final double LeftBack = r * Math.sin(robotAngle)-rightX;
+        final double RightBack = r * Math.cos(robotAngle)+rightX;
+
+
+
+
+//        double HeadingAngle = Math.atan2(xDirection, yDirection);
+//        double speed = Math.sqrt(Math.pow(xDirection, 2) + Math.pow(yDirection, 2));
+//
+//        double RightFront = speed * Math.cos((90 - HeadingAngle - 45) / 180 * Math.PI);
+//        double RightBack = speed * Math.cos((90 - HeadingAngle - 135) / 180 * Math.PI);
+//        double LeftBack = speed * Math.cos((90 - HeadingAngle - 225) / 180 * Math.PI);
+//        double LeftFront = speed * Math.cos((90 - HeadingAngle - 315) / 180 * Math.PI);
+//
+        m_drivetrain.right_front.setPower(RightFront);
+        m_drivetrain.right_back.setPower(RightBack);
+        m_drivetrain.left_back.setPower(LeftBack);
+        m_drivetrain.left_front.setPower(LeftFront);
+
+
+    }
+    public void intake(Drivetrain m_Drivetrain, boolean intake){
+        if (intake) {
+            m_Drivetrain.intake_motor.setPower(1);
+            m_Drivetrain.intake2_motor.setPower(1);
+        } else{
+            m_Drivetrain.intake_motor.setPower(0);
+            m_Drivetrain.intake2_motor.setPower(0);
+        }
+    }
+
+}
